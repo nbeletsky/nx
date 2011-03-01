54d4c83998c4e9e9f2f9a8405a43749e15826289
Nick Sinopoli <NSinopoli@gmail.com>
2011-02-28 16:26:25 -0800

    More cleanup.

--- model/ApplicationModel.php
+++ model/ApplicationModel.php
@@ -6,14 +6,18 @@ class ApplicationModel extends \core\Model
 
     public function __construct($id=null, $repository=null) 
     {
-        // TODO: Fix
-        /*
         if ( is_null($repository) )
         {
-            $repository = $this->_d
+            $repository = $this->_get_default_repository();
         }
         parent::__construct($id, $repository);
-        */
+    }
+
+    private function _get_default_repository()
+    {
+        $db = new PDO_MySQL(DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASS);
+        $cache = new MemcachedCache();
+        return new Repository($db, $cache); 
     }
 
 }
