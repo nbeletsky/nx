f6bd6f9fe776df0d2d8505b2845317902f9d8e32
Nick Sinopoli <NSinopoli@gmail.com>
2011-05-14 18:25:38 -0700

    Fix namespace issue.

--- app/public/index.php
+++ app/public/index.php
@@ -5,7 +5,7 @@
     require "../config/config.php";
 
     $args = \nx\lib\Dispatcher::parse_query_string($_SERVER['QUERY_STRING']);
-    $args['post'] = ( !empty($_POST) ) ? Data::extract_post($_POST) : array();
+    $args['post'] = ( !empty($_POST) ) ? \nx\lib\Data::extract_post($_POST) : array();
 
     \nx\lib\Dispatcher::render($args);
 
