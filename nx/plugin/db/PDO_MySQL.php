9dc441b93ae464a96b9e04714cc796c2ed52b097
Nick Sinopoli <NSinopoli@gmail.com>
2011-05-27 12:14:00 -0700

    Remove debugging info.

--- nx/plugin/db/PDO_MySQL.php
+++ nx/plugin/db/PDO_MySQL.php
@@ -419,7 +419,6 @@ class PDO_MySQL extends \nx\core\Object {
             $statement->execute();
     	} catch ( \PDOException $e ) {
             // TODO: How to handle error reporting?
-            die($e->getMessage() . $sql . var_dump($properties) . var_dump($e->getTrace()));
             $this->_affected_rows = 0;
             return false;
         }
