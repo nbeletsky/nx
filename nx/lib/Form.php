aaf4d775378fa4ecd9d3e7b7320a812753edd4f7
Nick Sinopoli <NSinopoli@gmail.com>
2011-10-29 16:58:31 -0400

    Update form, form test.

--- nx/lib/Form.php
+++ nx/lib/Form.php
@@ -147,7 +147,7 @@ class Form {
     * @access protected
     * @return string
     */
-    protected function _parse_attributes($attributes, $binding, $options = array()) {
+    protected function _parse_attributes($attributes, $binding = null, $options = array()) {
         $options += array(
             'is_radio'    => false,
             'is_select'   => false,
@@ -159,7 +159,7 @@ class Form {
             // An attribute passed alone without a key (e.g., array('autofocus'))
             // will be assigned a numeric key by PHP
             if ( is_numeric($key) ) {
-                $html .= $setting . " ";
+                $html .= $this->escape($setting) . " ";
                 continue;
             }
 
@@ -178,18 +178,22 @@ class Form {
                         break;
                     }
 
-                    $object_id = spl_object_hash($binding);
-                    if ( !in_array($object_id, $this->_binding_counter) ) {
-                        $this->_binding_counter[] = $object_id;
+                    if ( !array_key_exists($b_class, $this->_binding_counter) ) {
+                        $this->_binding_counter[$b_class] = array();
                     }
-                    $index = array_search($object_id, $this->_binding_counter);
+
+                    $object_id = spl_object_hash($binding);
+                    if ( !in_array($object_id, $this->_binding_counter[$b_class]) ) {
+                        $this->_binding_counter[$b_class][] = $object_id;
+                    }
+                    $index = array_search($object_id, $this->_binding_counter[$b_class]);
                     $setting = $b_class . '[' . $index . '][' . $setting . "]";
                     break;
                 case 'value':
                     $value_present = true;
                     break;
             }
-            $html .= $key . "='" . $this->escape($setting) . "' ";
+            $html .= $this->escape($key) . "='" . $this->escape($setting) . "' ";
         }
 
         if (
@@ -212,23 +216,43 @@ class Form {
     * Creates a set of radio buttons.
     *
     * @param array $attributes          The HTML attributes.
-    * @param array $values              The values to use for the radio buttons.
-    *                                   Must be in the format of:
+    *                                   Key 'id' takes an array of ids, and
+    *                                   key 'value' can be in the format of:
     *                                   'value' => 'display'
     *                                   (radio value will be set to 'value',
     *                                   and the text to the right of the
     *                                   radio will be set to 'display')
+    *                                   or as an array without keys
+    *                                   (both radio value and the text to the
+    *                                   right of the radio will be set to the
+    *                                   passed value)
     * @param obj $binding               The object to which the value of the
     *                                   radio buttons should be mapped.
     * @access public
     * @return string
     */
-    public function radios($attributes, $values = array(), $binding = null) {
+    public function radios($attributes, $binding = null) {
         $options = array('is_radio' => true);
         $html = '';
+        $values = $attributes['value'];
+        unset($attributes['value']);
+        // Array is not associative
+        if ( $values === array_values($values) ) {
+            $values = array_combine($values, $values);
+        }
+        if ( isset($attributes['id']) ) {
+            $ids = $attributes['id'];
+            unset($attributes['id']);
+        }
+        $index = 0;
         foreach ( $values as $value => $display ) {
             $html .= "<input type='radio' ";
             $html .= $this->_parse_attributes($attributes, $binding, $options);
+            $label_for = '';
+            if ( isset($ids[$index]) ) {
+                $html .= "id='" . strtolower($ids[$index]) . "' ";
+                $label_for = " for='" . strtolower($ids[$index]) . "'";
+            }
             $html .= "value='" . $this->escape($value) . "' ";
             if (
                 !is_null($binding)
@@ -238,7 +262,10 @@ class Form {
             ) {
                 $html .= "checked='checked' ";
             }
-            $html.= "/>" . $display;
+            $html .= "/> ";
+            $html .= "<label" . $label_for;
+            $html .= ">" . $display . "</label>";
+            $index++;
         }
 
         return $html;
@@ -278,8 +305,12 @@ class Form {
         $html = "<select ";
         $html .= $this->_parse_attributes($attributes, $binding, $element_options);
         $html = rtrim($html) . ">";
+        // Array is not associative
+        if ( $options === array_values($options) ) {
+            $options = array_combine($options, $options);
+        }
         foreach( $options as $value => $display ) {
-            $html.= "<option value='" . $this->escape($value) . "' ";
+            $html .= "<option value='" . $this->escape($value) . "' ";
 
             if (
                 !is_null($binding)
@@ -287,11 +318,11 @@ class Form {
                 && property_exists($binding, $attributes['name'])
                 && $binding->$attributes['name'] == $value
             ) {
-                $html.= "selected='selected' ";
+                $html .= "selected='selected' ";
             }
             $html = rtrim($html) . ">" . $this->escape($display) . "</option>";
         }
-        $html.= "</select>";
+        $html .= "</select>";
 
         return $html;
     }
--- nx/test/lib/FormTest.php
+++ nx/test/lib/FormTest.php
@@ -13,6 +13,15 @@ class ModelMock extends \nx\core\Model {
     }
 }
 
+class ModelMock2 extends \nx\core\Model {
+    protected $id;
+    protected $test_name = 'test value';
+
+    public function set_id($val) {
+        $this->id = $val;
+    }
+}
+
 class FormTest extends \PHPUnit_Framework_TestCase {
 
     protected $_form;
@@ -57,33 +66,43 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         $check = "<textarea id='test_id' class='test_class' name='test_name'>test value</textarea>";
         $this->assertEquals($check, $input);
 
-        $attributes = array('name'  => 'test_name');
-        $values = array(
-            'green'  => 'green',
-            'orange' => 'orange',
-            'red'    => 'red',
-            'blue'   => 'blue'
+        $attributes = array(
+            'name'  => 'test_name',
+            'value' => array(
+                'green'  => 'green',
+                'orange' => 'orange',
+                'red'    => 'red',
+                'blue'   => 'blue'
+            )
         );
 
-        $input = $this->_form->radios($attributes, $values);
-        $check = "<input type='radio' name='test_name' value='green' />green"
-            . "<input type='radio' name='test_name' value='orange' />orange"
-            . "<input type='radio' name='test_name' value='red' />red"
-            . "<input type='radio' name='test_name' value='blue' />blue";
+        $input = $this->_form->radios($attributes);
+        $check = "<input type='radio' name='test_name' value='green' /> <label>green</label>"
+            . "<input type='radio' name='test_name' value='orange' /> <label>orange</label>"
+            . "<input type='radio' name='test_name' value='red' /> <label>red</label>"
+            . "<input type='radio' name='test_name' value='blue' /> <label>blue</label>";
         $this->assertEquals($check, $input);
 
-        $values = array(
+        $attributes['value'] = array(
             '17' => 'green',
             '18' => 'orange',
             '19' => 'red',
             '20' => 'blue'
         );
 
-        $input = $this->_form->radios($attributes, $values);
-        $check = "<input type='radio' name='test_name' value='17' />green"
-            . "<input type='radio' name='test_name' value='18' />orange"
-            . "<input type='radio' name='test_name' value='19' />red"
-            . "<input type='radio' name='test_name' value='20' />blue";
+        $input = $this->_form->radios($attributes);
+        $check = "<input type='radio' name='test_name' value='17' /> <label>green</label>"
+            . "<input type='radio' name='test_name' value='18' /> <label>orange</label>"
+            . "<input type='radio' name='test_name' value='19' /> <label>red</label>"
+            . "<input type='radio' name='test_name' value='20' /> <label>blue</label>";
+        $this->assertEquals($check, $input);
+
+        $attributes['id'] = array('one', 'two', 'three', 'four');
+        $input = $this->_form->radios($attributes);
+        $check = "<input type='radio' name='test_name' id='one' value='17' /> <label for='one'>green</label>"
+            . "<input type='radio' name='test_name' id='two' value='18' /> <label for='two'>orange</label>"
+            . "<input type='radio' name='test_name' id='three' value='19' /> <label for='three'>red</label>"
+            . "<input type='radio' name='test_name' id='four' value='20' /> <label for='four'>blue</label>";
         $this->assertEquals($check, $input);
 
         $attributes = array(
@@ -301,41 +320,59 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         $binding = new ModelMock();
 
         // Name set as the property of our object
-        $attributes = array('name'  => 'test_name');
-        $values = array(
-            'green'      => 'green',
-            'orange'     => 'orange',
-            'test value' => 'yellow',
-            'red'        => 'red',
-            'blue'       => 'blue'
+        $attributes = array(
+            'name'  => 'test_name',
+            'value' => array(
+                'green'      => 'green',
+                'orange'     => 'orange',
+                'test value' => 'yellow',
+                'red'        => 'red',
+                'blue'       => 'blue'
+            )
         );
 
-        $input = $this->_form->radios($attributes, $values, $binding);
-        $check = "<input type='radio' name='ModelMock[0][test_name]' value='green' />green"
-            . "<input type='radio' name='ModelMock[0][test_name]' value='orange' />orange"
-            . "<input type='radio' name='ModelMock[0][test_name]' value='test value' checked='checked' />yellow"
-            . "<input type='radio' name='ModelMock[0][test_name]' value='red' />red"
-            . "<input type='radio' name='ModelMock[0][test_name]' value='blue' />blue";
+        $input = $this->_form->radios($attributes, $binding);
+        $check = "<input type='radio' name='ModelMock[0][test_name]' value='green' /> <label>green</label>"
+            . "<input type='radio' name='ModelMock[0][test_name]' value='orange' /> <label>orange</label>"
+            . "<input type='radio' name='ModelMock[0][test_name]' value='test value' checked='checked' /> <label>yellow</label>"
+            . "<input type='radio' name='ModelMock[0][test_name]' value='red' /> <label>red</label>"
+            . "<input type='radio' name='ModelMock[0][test_name]' value='blue' /> <label>blue</label>";
         $this->assertEquals($check, $input);
 
         // Name different from the property of our object
-        $attributes = array('name'  => 'test');
-        $values = array(
-            '17'         => 'green',
-            '18'         => 'orange',
-            'test value' => 'yellow',
-            '19'         => 'red',
-            '20'         => 'blue'
+        $attributes = array(
+            'name'  => 'test',
+            'value' => array(
+                '17'         => 'green',
+                '18'         => 'orange',
+                'test value' => 'yellow',
+                '19'         => 'red',
+                '20'         => 'blue'
+            )
         );
 
-        $input = $this->_form->radios($attributes, $values, $binding);
-        $check = "<input type='radio' name='ModelMock[0][test]' value='17' />green"
-            . "<input type='radio' name='ModelMock[0][test]' value='18' />orange"
-            . "<input type='radio' name='ModelMock[0][test]' value='test value' />yellow"
-            . "<input type='radio' name='ModelMock[0][test]' value='19' />red"
-            . "<input type='radio' name='ModelMock[0][test]' value='20' />blue";
+        $input = $this->_form->radios($attributes, $binding);
+        $check = "<input type='radio' name='ModelMock[0][test]' value='17' /> <label>green</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='18' /> <label>orange</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='test value' /> <label>yellow</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='19' /> <label>red</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='20' /> <label>blue</label>";
         $this->assertEquals($check, $input);
 
+        // Name different from the property of our object
+        // and values as a non-associative array
+        $attributes = array(
+            'name'  => 'test',
+            'value' => array('green', 'orange', 'yellow', 'red', 'blue')
+        );
+
+        $input = $this->_form->radios($attributes, $binding);
+        $check = "<input type='radio' name='ModelMock[0][test]' value='green' /> <label>green</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='orange' /> <label>orange</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='yellow' /> <label>yellow</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='red' /> <label>red</label>"
+            . "<input type='radio' name='ModelMock[0][test]' value='blue' /> <label>blue</label>";
+        $this->assertEquals($check, $input);
     }
 
     public function test_SelectWithBindingsWithoutId_ReturnsHtml() {
@@ -379,17 +416,30 @@ class FormTest extends \PHPUnit_Framework_TestCase {
             . "<option value='19'>red</option><option value='20'>blue</option>"
             . "</select>";
         $this->assertEquals($check, $input);
+
+        // Name different from the property of our object
+        // and values as a non-associative array
+        $attributes = array('name'  => 'test');
+        $values = array('green', 'orange', 'yellow', 'red', 'blue');
+
+        $input = $this->_form->select($attributes, $values, $binding);
+        $check = "<select name='ModelMock[0][test]'>"
+            . "<option value='green'>green</option><option value='orange'>orange</option>"
+            . "<option value='yellow'>yellow</option>"
+            . "<option value='red'>red</option><option value='blue'>blue</option>"
+            . "</select>";
+        $this->assertEquals($check, $input);
     }
 
     public function test_MultipleBindingsWithoutId_ReturnsHtml() {
-        $binding_1 = new ModelMock();
-
         $attributes = array(
             'id'    => 'test_id',
             'class' => 'test_class',
             'name'  => 'test_name',
             'value' => 'test value'
         );
+
+        $binding_1 = new ModelMock();
 
         $input = $this->_form->text($attributes, $binding_1);
         $check = "<input type='text' id='test_id' class='test_class' name='ModelMock[0][test_name]' value='test value' />";
@@ -410,22 +460,39 @@ class FormTest extends \PHPUnit_Framework_TestCase {
 
         $binding_4 = new ModelMock();
 
-        $attributes = array('name'  => 'test');
-        $values = array(
-            '17'         => 'green',
-            '18'         => 'orange',
-            'test value' => 'yellow',
-            '19'         => 'red',
-            '20'         => 'blue'
+        $attributes = array(
+            'name'  => 'test',
+            'value' => array(
+                '17'         => 'green',
+                '18'         => 'orange',
+                'test value' => 'yellow',
+                '19'         => 'red',
+                '20'         => 'blue'
+            ),
+            'id'    => array('one', 'two', 'three', 'four', 'five')
         );
 
-        $input = $this->_form->radios($attributes, $values, $binding_4);
-        $check = "<input type='radio' name='ModelMock[3][test]' value='17' />green"
-            . "<input type='radio' name='ModelMock[3][test]' value='18' />orange"
-            . "<input type='radio' name='ModelMock[3][test]' value='test value' />yellow"
-            . "<input type='radio' name='ModelMock[3][test]' value='19' />red"
-            . "<input type='radio' name='ModelMock[3][test]' value='20' />blue";
+        $input = $this->_form->radios($attributes, $binding_4);
+        $check = "<input type='radio' name='ModelMock[3][test]' id='one' value='17' /> <label for='one'>green</label>"
+            . "<input type='radio' name='ModelMock[3][test]' id='two' value='18' /> <label for='two'>orange</label>"
+            . "<input type='radio' name='ModelMock[3][test]' id='three' value='test value' /> <label for='three'>yellow</label>"
+            . "<input type='radio' name='ModelMock[3][test]' id='four' value='19' /> <label for='four'>red</label>"
+            . "<input type='radio' name='ModelMock[3][test]' id='five' value='20' /> <label for='five'>blue</label>";
         $this->assertEquals($check, $input);
+
+        // Test with another object
+        $binding_other = new ModelMock2();
+        $attributes = array(
+            'id'    => 'test_id',
+            'class' => 'test_class',
+            'name'  => 'test_name',
+            'value' => 'test value'
+        );
+
+        $input = $this->_form->text($attributes, $binding_other);
+        $check = "<input type='text' id='test_id' class='test_class' name='ModelMock2[0][test_name]' value='test value' />";
+        $this->assertEquals($check, $input);
+
     }
 
     public function test_BindingsWithId_ReturnsHtml() {
@@ -451,6 +518,31 @@ class FormTest extends \PHPUnit_Framework_TestCase {
         );
         $input = $this->_form->email($attributes, $binding);
         $check = "<input type='email' id='test_id' class='test_class' name='ModelMock|27[test_name_2]' value='test' />";
+        $this->assertEquals($check, $input);
+
+        // Test with another object
+        $binding = new ModelMock2();
+        $binding->set_id(38);
+
+        // Test with a custom value
+        $attributes = array(
+            'id'    => 'test_id',
+            'class' => 'test_class',
+            'name'  => 'test_name',
+            'value' => 'test'
+        );
+        $input = $this->_form->checkbox($attributes, $binding);
+        $check = "<input type='checkbox' id='test_id' class='test_class' name='ModelMock2|38[test_name]' value='test' />";
+        $this->assertEquals($check, $input);
+
+        $attributes = array(
+            'id'    => 'test_id',
+            'class' => 'test_class',
+            'name'  => 'test_name_2',
+            'value' => 'test'
+        );
+        $input = $this->_form->email($attributes, $binding);
+        $check = "<input type='email' id='test_id' class='test_class' name='ModelMock2|38[test_name_2]' value='test' />";
         $this->assertEquals($check, $input);
     }
 
